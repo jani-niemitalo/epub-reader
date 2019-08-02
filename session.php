ce8d14e44c1c32638fe6713add36a4371c1ef94a
Aqua <jani.niemitalo@gmail.com>
2019-08-03 00:26:32 +0300

    fixed

--- session.php
+++ session.php
@@ -7,12 +7,6 @@
  */
 require_once("mysqlConnection.php");
 
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}
 if (!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] !== true) {
     //echo "Login First!";
     header("Location: login.php");
